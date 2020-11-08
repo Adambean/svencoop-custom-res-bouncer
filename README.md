66fbe7c62fce0a662ae2cf0d9739a97bb8597760
Adam Reece <adam@reece.wales>
2020-11-08 12:00:20 +0000

    Initial Bash implementation.

---
+++ README.md
@@ -0,0 +1,122 @@
+# Sven Co-op custom resource bouncer
+
+This tool will mitigate the blight of uncreative Sven Co-op server operators bombarding your game with truckloads of player models and sounds you do not want. It takes in lists of download paths of resources you don't want and replaces them with either the stock "helmet" player model or an empty file. This prevents Sven Co-op from downloading them again in game because the files already exist, as it refuses to overwrite existing files during content downloading.
+
+In effect you're bouncing those custom resources you don't want away from your game.
+
+It will not overrule any official content, or any content you've intentionally installed into your "addon" content folder. Only the "downloads" folder used for in game downloads will be scanned and modified.
+
+## Prerequisites
+
+This is a command line utility currently available for Bash for both Linux and Cygwin for Windows. It requires use of:
+
+* basename
+* cd
+* cp
+* dirname
+* find
+* mkdir
+* printf
+* pwd
+* read
+* realpath
+* rm
+* sed
+* sort
+* touch
+
+A Windows batch command script is in progress.
+
+## Getting Started
+
+First you need to define a list of player models and/or sounds you want to filter out. You don't have to define both.
+
+### Filtering player models
+
+Create a text file called "filter-player-models.txt" in the same directory as this tool. There is an example file called "filter-player-models.example.txt" you could copy to get started, containing just one entry as an example.
+
+The contents of this file is a simple list of player models you do not wish to see in game split by lines.
+
+For example:
+
+```
+anon
+rage_admin
+typical_anime_girl_31a
+```
+
+
+### Filtering sounds
+
+Create a text file called "filter-sounds.txt" in the same directory as this tool. There is an example file called "filter-sounds.example.txt" you could copy to get started, containing just one entry as an example.
+
+The contents of this file is a list of directories and/or files you do not wish to hear in game split by lines relative to the "sound" game resource path.
+
+For example:
+
+```
+chatsounds/
+music/cheesyloadingtrack05.mp3
+voicecommands/
+```
+
+All listed files, and files found in directories recursively, will be replaced by an empty file.
+
+If you're specifying a whole directory you want to filter out you must end your line with a forward slash `/`. This matters because it determines what should be done if the directory doesn't yet exist. (Omitting the `/` won't cause a problem, it'll just limit the functionality of this tool.)
+
+## Running
+
+To run this tool open a command prompt or terminal, then execute it passing a path to your "Sven Co-op" folder as argument 1.
+
+For example:
+
+    `./deploy.sh ~/.steam/steamapps/common/Sven\ Co-op`
+    `./deploy.sh /cygdrive/C/Program\ Files\ (x86)/Steam/steamapps/common/Sven\ Co-op`
+    `deploy.cmd "C:\Program Files (x86)\Steam\Steamapps\common\Sven Co-op"` (coming soon)
+
+Listed player models will be replaced by the stock "helmet" model regardless of whether they already exist or not.
+
+Listed sounds are handled differently depending on whether they are an exact file path or directory.
+
+#### When handling exact file paths
+
+For each file its directory is checked for existence. If it doesn't exist it'll be created, and an empty file will be created within. If the directory did exist but the file did not, then empty file will be created. If the file already exists it will be deleted then recreated as an empty file.
+
+Using exact file paths allows you to pre-emptively block sounds before they first arrive, but requires you to know in advance what the file names will be.
+
+#### When handling directories:
+
+If the directory doesn't yet exist it will be created. No further action can be taken in this case because the tool cannot pre-emptively guess what sound files will arrive. If you know the directory will exist after joining a particular server you should join that server once, allow downloading to complete, then run this tool again.
+
+If the directory exists it will be scanned recursively for all files within. Each file will be deleted then recreated as an empty file.
+
+## Built With
+
+* [Bash](https://www.gnu.org/software/bash/)
+* [Batch file](https://en.wikipedia.org/wiki/Batch_file) (coming soon)
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags).
+
+## Authors
+
+* **Adam Reece** - *Initial work* - [Adambean](https://github.com/Adambean)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+Copyright 2020 Adam Reece
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the [License](LICENSE) for the specific language governing permissions and limitations under the License.
---
+++ deploy.sh
@@ -0,0 +1,244 @@
+#!/bin/bash
+
+printf "Sven Co-op custom resource bouncer\n\n"
+
+
+
+# Config
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+GAME_DIR_PATHNAME=$(realpath "${DIR}/../..")
+if [ -n "${1}" ] && [ ! -z "${1}" ]; then
+	if [ ! -d "${1}" ]; then
+		printf "[Error] Game directory specified not found.\n"
+		exit 1
+	fi
+
+	GAME_DIR_PATHNAME=$(realpath "${1}")
+fi
+
+GAME_BASE_DIR_PATHNAME=$(realpath "${GAME_DIR_PATHNAME}/svencoop")
+GAME_BASE_DIR_NAME=$(basename "${GAME_BASE_DIR_PATHNAME}")
+
+GAME_ADDON_DIR_PATHNAME=$(realpath "${GAME_BASE_DIR_PATHNAME}_addon")
+GAME_ADDON_DIR_NAME=$(basename "${GAME_ADDON_DIR_PATHNAME}")
+
+GAME_DOWNLOADS_DIR_PATHNAME=$(realpath "${GAME_BASE_DIR_PATHNAME}_downloads")
+GAME_DOWNLOADS_DIR_NAME=$(basename "${GAME_DOWNLOADS_DIR_PATHNAME}")
+
+REPLACEMENT_RESOURCES_DIR_PATHNAME=$(realpath "${DIR}/resources")
+
+REPLACEMENT_PLAYER_MODEL_NAME="helmet"
+REPLACEMENT_PLAYER_MODEL_DIR_PATHNAME=$(realpath "${GAME_BASE_DIR_PATHNAME}/models/player/${REPLACEMENT_PLAYER_MODEL_NAME}")
+
+FILTER_PLAYER_MODELS_FILE_PATHNAME=$(realpath "${DIR}/filter-player-models.txt")
+FILTER_SOUNDS_FILE_PATHNAME=$(realpath "${DIR}/filter-sounds.txt")
+
+
+
+# Information
+
+printf "Directory path names:\n"
+printf " - Tool                         ${DIR}\n"
+printf " - Game                         ${GAME_DIR_PATHNAME}\n"
+printf " - Base content                 ${GAME_BASE_DIR_PATHNAME}\n"
+printf " - Add-on content               ${GAME_ADDON_DIR_PATHNAME}\n"
+printf " - Downloaded content           ${GAME_DOWNLOADS_DIR_PATHNAME}\n"
+printf "\n"
+
+printf "Player model sub:               ${REPLACEMENT_PLAYER_MODEL_NAME}\n"
+printf "Player model filter:            ${FILTER_PLAYER_MODELS_FILE_PATHNAME}\n"
+printf "Sounds filter:                  ${FILTER_SOUNDS_FILE_PATHNAME}\n"
+printf "\n"
+
+
+
+# Pre-flight checks
+
+if [ ! -d "${GAME_DIR_PATHNAME}" ]; then
+	printf "[Error] Game directory not found.\n"
+	exit 1
+fi
+
+if [ ! -d "${GAME_BASE_DIR_PATHNAME}" ]; then
+	printf "[Error] Game base directory not found.\n"
+	exit 1
+fi
+
+if [ ! -f "${GAME_BASE_DIR_PATHNAME}/liblist.gam" ]; then
+	printf "[Error] Game base content directory invalid..\n"
+	exit 1
+fi
+
+if [ ! -f "${GAME_ADDON_DIR_PATHNAME}/../${GAME_BASE_DIR_NAME}/liblist.gam" ]; then
+	printf "[Error] Game add-on content directory invalid.\n"
+	exit 1
+fi
+
+if [ ! -d "${GAME_DOWNLOADS_DIR_PATHNAME}" ]; then
+	printf "[Info] Game downloaded content directory not found. It will be created.\n"
+	mkdir -p "${GAME_DOWNLOADS_DIR_PATHNAME}"
+
+	if [ ! -d "${GAME_DOWNLOADS_DIR_PATHNAME}" ]; then
+		printf "[Error] Game downloaded content directory could not be created.\n"
+		exit 1
+	fi
+fi
+
+if [ ! -f "${GAME_DOWNLOADS_DIR_PATHNAME}/../${GAME_BASE_DIR_NAME}/liblist.gam" ]; then
+	printf "[Error] Game downloaded content directory invalid.\n"
+	exit 1
+fi
+
+if [ ! -d "${REPLACEMENT_PLAYER_MODEL_DIR_PATHNAME}" ]; then
+	printf "[Error] Replacement player model directory \"${REPLACEMENT_PLAYER_MODEL_NAME}\" not found.\n"
+	exit 1
+fi
+
+if [ ! -f "${REPLACEMENT_PLAYER_MODEL_DIR_PATHNAME}/${REPLACEMENT_PLAYER_MODEL_NAME}.mdl" ]; then
+	printf "[Error] Replacement player model file \"${REPLACEMENT_PLAYER_MODEL_NAME}\" not found.\n"
+	exit 1
+fi
+
+if [ ! -f "${REPLACEMENT_PLAYER_MODEL_DIR_PATHNAME}/${REPLACEMENT_PLAYER_MODEL_NAME}.bmp" ]; then
+	printf "[Warning] Replacement player model file \"${REPLACEMENT_PLAYER_MODEL_NAME}\" thumbnail not found.\n"
+fi
+
+
+
+# Prepare resource directories
+
+printf "\nPreparing resource directories...\n\n"
+
+if [ ! -d "${GAME_ADDON_DIR_PATHNAME}/models/player" ]; then
+	mkdir -p "${GAME_ADDON_DIR_PATHNAME}/models/player"
+fi
+
+if [ ! -d "${GAME_ADDON_DIR_PATHNAME}/sound" ]; then
+	mkdir -p "${GAME_ADDON_DIR_PATHNAME}/sound"
+fi
+
+if [ ! -d "${GAME_DOWNLOADS_DIR_PATHNAME}/models/player" ]; then
+	mkdir -p "${GAME_DOWNLOADS_DIR_PATHNAME}/models/player"
+fi
+
+if [ ! -d "${GAME_DOWNLOADS_DIR_PATHNAME}/sound" ]; then
+	mkdir -p "${GAME_DOWNLOADS_DIR_PATHNAME}/sound"
+fi
+
+
+
+# Filter player models
+
+printf "\nFiltering player models...\n\n"
+
+if [ -f "${FILTER_PLAYER_MODELS_FILE_PATHNAME}" ]; then
+	if [ -s "${FILTER_PLAYER_MODELS_FILE_PATHNAME}" ]; then
+		while read FILTER_PLAYER_MODEL_NAME; do
+			FILTER_PLAYER_MODEL_NAME=$(sed 's/\r$//' <<< $FILTER_PLAYER_MODEL_NAME)
+			if [ ! -n "${FILTER_PLAYER_MODEL_NAME}" ] || [ -z "${FILTER_PLAYER_MODEL_NAME}" ]; then
+				continue
+			fi
+
+			printf "${FILTER_PLAYER_MODEL_NAME}:\n"
+
+			FILTER_PLAYER_MODEL_ADDON_DIR_PATHNAME=$(realpath "${GAME_ADDON_DIR_PATHNAME}/models/player/${FILTER_PLAYER_MODEL_NAME}")
+			FILTER_PLAYER_MODEL_DOWNLOADS_DIR_PATHNAME=$(realpath "${GAME_DOWNLOADS_DIR_PATHNAME}/models/player/${FILTER_PLAYER_MODEL_NAME}")
+
+			if [ -d "${FILTER_PLAYER_MODEL_DOWNLOADS_DIR_PATHNAME}" ]; then
+				printf " - Deleting directory from downloads.\n"
+				rm -rf "${FILTER_PLAYER_MODEL_DOWNLOADS_DIR_PATHNAME}"
+			fi
+
+			if [ ! -d "${FILTER_PLAYER_MODEL_ADDON_DIR_PATHNAME}" ]; then
+				mkdir -p "${FILTER_PLAYER_MODEL_ADDON_DIR_PATHNAME}"
+			fi
+
+			find "${REPLACEMENT_PLAYER_MODEL_DIR_PATHNAME}" -type f | sort | while read REPLACEMENT_PLAYER_MODEL_FILE_PATHNAME; do
+				cp "${REPLACEMENT_PLAYER_MODEL_FILE_PATHNAME}" "${FILTER_PLAYER_MODEL_ADDON_DIR_PATHNAME}/"
+			done
+
+			printf " - Filter installed.\n"
+		done < "${FILTER_PLAYER_MODELS_FILE_PATHNAME}"
+	else
+		printf "[Warning] Player models filter file is empty.\n"
+	fi
+else
+	printf "[Warning] Player models filter file not found.\n"
+fi
+
+
+
+# Filter sounds
+
+printf "\nFiltering sounds...\n\n"
+
+if [ -f "${FILTER_SOUNDS_FILE_PATHNAME}" ]; then
+	if [ -s "${FILTER_SOUNDS_FILE_PATHNAME}" ]; then
+		while read FILTER_SOUND_NAME; do
+			FILTER_SOUND_NAME=$(sed 's/\r$//' <<< $FILTER_SOUND_NAME)
+			if [ ! -n "${FILTER_SOUND_NAME}" ] || [ -z "${FILTER_SOUND_NAME}" ]; then
+				continue
+			fi
+
+			printf "${FILTER_SOUND_NAME}:\n"
+
+			FILTER_SOUND_DOWNLOADS_PATHNAME=$(realpath "${GAME_DOWNLOADS_DIR_PATHNAME}/sound/${FILTER_SOUND_NAME}")
+			declare -a FILTER_SOUND_REPLACEMENT_FILE_PATHNAMES
+
+			if [ -d "${FILTER_SOUND_DOWNLOADS_PATHNAME}" ]; then
+				while read FILTER_SOUND_DOWNLOADS_SUBITEM_PATHNAME; do
+					FILTER_SOUND_REPLACEMENT_FILE_PATHNAMES+=( "${FILTER_SOUND_DOWNLOADS_SUBITEM_PATHNAME}" )
+					#printf " - Detected file to filter: ${FILTER_SOUND_DOWNLOADS_SUBITEM_PATHNAME}\n"
+				done < <(find "${FILTER_SOUND_DOWNLOADS_PATHNAME}" -type f | sort)
+			elif [ -f "${FILTER_SOUND_DOWNLOADS_PATHNAME}" ]; then
+				FILTER_SOUND_REPLACEMENT_FILE_PATHNAMES+=( "${FILTER_SOUND_DOWNLOADS_PATHNAME}" )
+				#printf " - Detected file to filter: ${FILTER_SOUND_DOWNLOADS_PATHNAME}\n"
+			else
+				if [ "${FILTER_SOUND_NAME: -1}" != "/" ]; then
+					FILTER_SOUND_REPLACEMENT_FILE_PATHNAMES+=( "${FILTER_SOUND_DOWNLOADS_PATHNAME}" )
+				fi
+				#printf " - No downloaded directory or file detected to filter.\n"
+			fi
+
+			if [ "${FILTER_SOUND_NAME: -1}" == "/" ]; then
+				if [ ! -d "${FILTER_SOUND_DOWNLOADS_PATHNAME}" ]; then
+					mkdir -p "${FILTER_SOUND_DOWNLOADS_PATHNAME}"
+				fi
+			else
+				if [ ! -f "${FILTER_SOUND_DOWNLOADS_PATHNAME}" ]; then
+					mkdir -p $(dirname "${FILTER_SOUND_DOWNLOADS_PATHNAME}")
+				fi
+			fi
+
+			for FILTER_SOUND_REPLACEMENT_PATHNAME in "${FILTER_SOUND_REPLACEMENT_FILE_PATHNAMES[@]}"; do
+				if [ -f "${FILTER_SOUND_REPLACEMENT_PATHNAME}" ]; then
+					if [ -s "${FILTER_SOUND_REPLACEMENT_PATHNAME}" ]; then
+						rm -f "${FILTER_SOUND_REPLACEMENT_PATHNAME}"
+						touch "${FILTER_SOUND_REPLACEMENT_PATHNAME}"
+
+						printf " - Filter installed: ${FILTER_SOUND_REPLACEMENT_PATHNAME}\n"
+					else
+						printf " - Filter already installed: ${FILTER_SOUND_REPLACEMENT_PATHNAME}\n"
+					fi
+				else
+					touch "${FILTER_SOUND_REPLACEMENT_PATHNAME}"
+					printf " - Filter pre-emptively installed: ${FILTER_SOUND_REPLACEMENT_PATHNAME}\n"
+				fi
+			done
+
+			break
+		done < "${FILTER_SOUNDS_FILE_PATHNAME}"
+	else
+		printf "[Warning] Sounds filter file is empty.\n"
+	fi
+else
+	printf "[Warning] Sounds filter file not found.\n"
+fi
+
+
+
+# Finished
+
+exit 0
